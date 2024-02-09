789c88976f7f68265275d3029f4da285da502151
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-02-09 11:07:30 +0100

    Forgot Password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,20 +3,24 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use http\Env\Request;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
+use Mockery\Generator\StringManipulation\Pass\Pass;
 
 class ForgotPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset emails and
-    | includes a trait which assists in sending these notifications from
-    | your application to your users. Feel free to explore this trait.
-    |
-    */
+    public function forgotPassword(Request $request){
+
+        $request->validate(['email' => 'required|email']);
+        $status =Password::sendResentLink(
+            $request->only('email')
+        );
+        return $status === Password::RESET_LINK_SENT
+            ? response()->json(['message' => __($status)], 200)
+            : response()->json(['message' => __($status)], 400);
+
+    }
 
     use SendsPasswordResetEmails;
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('forgot-password', 'ForgotPasswordController@forgotPassword');
+
+Route::post('reset-password', 'ResetPasswordController@resetPassword');
