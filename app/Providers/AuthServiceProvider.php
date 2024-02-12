5e1558154f8b40834f5b0413632a0c3d83e50853
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-02-12 15:16:45 +0100

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Sanctum\Sanctum;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->registerPolicies();
+
+        Sanctum::usePersonalAccessTokenModel(SanctumPersonalAccessToken::class);
         Gate::define('admin', function (User $user): bool {
             //
         });
