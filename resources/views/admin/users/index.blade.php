d0ea457c4470f81656570ca6c115e3d21dd90605
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-02-15 16:38:43 +0100

    Update index.blade.php

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -31,15 +31,16 @@
                                 </tr>
                             @endforeach
                             </tbody>
+                            
                         </table>
+                        <a href="{{ route('generate-pdf') }}" class="btn btn-primary btn-lg mt-2  justify-content-center ">
+    Download users/roles DATA.PDF
+</a>
 
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <a href="{{ route('generate-pdf') }}" class="btn btn-primary btn-lg mt-2">
-    Download users/roles DATA.PDF
-</a>
 
 @endsection
