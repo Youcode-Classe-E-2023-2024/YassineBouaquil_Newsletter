9cb0ad177b0a06426df262ee88f2b89debba7bd8
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-02-15 16:45:39 +0100

    Update index.blade.php

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,46 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Liste Des Utilisateurs') }}</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Liste Des Utilisateurs') }}</div>
 
-                    <div class="card-body">
-                        <table class="table">
-                            <thead>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Nom</th>
                                 <th scope="col">Email</th>
                                 <th scope="col">Action</th>
                             </tr>
-                            </thead>
-                            <tbody>
+                        </thead>
+                        <tbody>
                             @foreach($users as $user)
-                                <tr>
-                                    <th scope="row">{{ $user->id }}</th>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>
-                                        <a href="{{ route('admin.users.edit', $user->id) }}"><button class="btn btn-primary">Editer</button></a>
-                                        <a href=""><button class="btn btn-danger">Edit Role</button></a>
-                                        <a href=""><button class="btn btn-primary">Supprimer</button></a>
-                                    </td>
-                                </tr>
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <a href="{{ route('admin.users.edit', $user->id) }}"><button class="btn btn-primary">Editer</button></a>
+                                    <a href=""><button class="btn btn-danger">Edit Role</button></a>
+                                    <a href=""><button class="btn btn-primary">Supprimer</button></a>
+                                </td>
+                            </tr>
                             @endforeach
-                            </tbody>
-                            
-                        </table>
-                        <a href="{{ route('generate-pdf') }}" class="btn btn-primary btn-lg mt-2  justify-content-center ">
-    Download users/roles DATA.PDF
-</a>
+                        </tbody>
 
-                    </div>
+                    </table>
+                    <a href="{{ route('generate-pdf') }}" class="btn btn-primary btn-lg mt-2  justify-content-center ">
+                        Download users/roles DATA.PDF
+                    </a>
+
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 @endsection
